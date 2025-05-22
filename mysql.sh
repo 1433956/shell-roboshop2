4453b68455ea8de3290aec5200c052048afffae0
raghu <raghumurukutla96@gmail.com>
2025-05-22 21:03:35 +0530

    new changes

---
+++ mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+check_root
+
+dnf install mysql-server -y &>> $LOG_FILE
+VALIDATE $? "installing my sql server"
+
+systemctl enable mysqld &>> $LOG_FILE
+VALIDATE $? "enable sql server"
+
+systemctl start mysqld &>> $LOG_FILE
+VALIDATE $? "starting my sql server"
+
+echo -e "$G please enter mysql Password: $N"
+read -s $MYSQL_ROOT_PASSWORD 
+
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+print_time
